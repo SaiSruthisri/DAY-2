3090c01139b5e9d89ac90a86a943a74e0e749a07
Sai Sruthisri <148372065+SaiSruthisri@users.noreply.github.com>
2024-06-14 23:40:44 +0530

    Add files via upload

---
+++ Code2.cpp
@@ -0,0 +1,44 @@
+class MinStack {
+public:
+    stack<int>stk;
+    stack<int>mini;
+
+    MinStack() {
+        //hi
+    }
+
+    void push(int val) {
+        stk.push(val);
+         if (mini.empty() || val <= mini.top())
+            mini.push(val);
+    }
+
+    void pop() {
+        if(stk.top()==mini.top())
+        {
+
+            mini.pop();
+        }
+        stk.pop();
+
+    }
+
+    int top() {
+        return stk.top();
+       }
+
+    int getMin() {
+
+        return mini.top();
+    }
+
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
